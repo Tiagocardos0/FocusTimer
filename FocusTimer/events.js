6519b839e4b82675a500ee861e8780ba13638861
Tiago Cardoso <tiago.card2121@gmail.com>
2024-06-15 16:04:54 -0300

    eventos
    Foi registrado os eventos dos botões

---
+++ FocusTimer/events.js
@@ -0,0 +1,9 @@
+import { controls } from './elements'
+
+export { controls } from './elements'
+
+export function registerControls() {
+    controls.addEventListener('click', (event) => {
+        
+    })
+}
